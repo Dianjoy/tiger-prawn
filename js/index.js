3615a9ab852621127168b0d5e143857b669911e7
Meathill <meathill@gmail.com>
2014-11-11 19:35:00 +0800

    基本完成demo

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
 $(function () {
-
+  $('.morris-data').each(function () {
+    var data = JSON.parse(this.innerHTML);
+    data.element = $(this).data('target');
+    new Morris.Line(data);
+  });
 });
