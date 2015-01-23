1e522a2518250d4d5e637e2a65c49f5af54d8fad
Meathill <meathill@gmail.com>
2015-01-23 17:40:32 +0800

    调整了创建广告的顺序，完成了抓取功能

--- js/component/AdEditor.js
+++ js/component/AdEditor.js
@@ -10,7 +10,8 @@
   ns.AdEditor = Backbone.View.extend({
     events: {
       'blur [name=ad_url]': 'adURL_blurHandler',
-      'click .platform label': 'platformButton_clickHandler',
+      'click .fetch-button': 'fetchButton_clickHandler',
+      'change [name=ad_app_type]': 'platform_changeHandler',
       'change .domestic input': 'area_changeHandler',
       'change .isp input': 'isp_changeHandler',
       'change #feedback': 'feedback_changeHandler',
@@ -19,6 +20,9 @@
     initialize: function () {
       var init = this.model.pick(_.keys(this.model.defaults));
       for (var key in init) {
+        if (!init.hasOwnProperty(key)) {
+          continue;
+        }
         var items = this.$('[name=' + key + '][value=' + init[key] + ']').prop('checked', true); // radio
         items.length > 0 || (items = this.$('[name="' + key + '[]"][value=' + init[key] + ']').prop('checked', true)); // checkbox
         items.length > 0 || this.$('[name=' + key + ']').val(init[key]); // select
@@ -28,14 +32,48 @@
       Backbone.View.prototype.remove.call(this);
       this.model.off(null, null, this);
     },
+    checkADURL: function (value) {
+      if (!value) {
+        this.$('.fetch-button')
+          .removeClass('btn-warning')
+          .prop('disabled', true);
+        return;
+      }
+      if (/\.ipa$/.test(value) || /itunes.apple.com/.test(value)) {
+        this.$('input[name=ad_app_type][value=2]').prop('checked', true);
+        return;
+      }
+      if (/\.apk$/.test(value)) {
+        this.$('.fetch-button')
+          .addClass('btn-warning')
+          .prop('disabled', false);
+        this.$('input[name=ad_app_type][value=1]').prop('checked', true);
+      }
+    },
     adURL_blurHandler: function (event) {
       if (this.$el.hasClass('iPhone') && event.target.value.substr(0, 12) !== IOS_PREFIX) {
         event.target.value = event.target.value.replace(/^https?:\/\//i, IOS_PREFIX);
       }
     },
+    // 根据内容选择平台
     adURL_changeHandler: function (event) {
-      var value = event.target.value;
-      if (!value) {
+      this.checkADURL(event.target.value);
+    },
+    area_changeHandler: function (event) {
+      var target = $(event.target);
+      this.$el.toggleClass(target.data('class'), target.val() === '1');
+    },
+    feedback_changeHandler: function (event) {
+      this.$el.toggleClass('show-feedback-detail', event.target.value === '2' || event.target.value === '3');
+    },
+    fetchButton_clickHandler: function () {
+      var field = this.$('[name=ad_url]')
+        , value = field.val();
+      if (!value || field.attr('type') === 'hidden') { // 用户上传的文件不抓包
+        return;
+      }
+      if (/itunes.apple.com/.test(value)) {
+        alert('App Store应用不需要抓取');
         return;
       }
       tp.service.Manager.call(tp.API + 'fetch/', {
@@ -46,13 +84,9 @@
         success: this.fetchFile_successHandler,
         error: this.fetchFile_errorHandler
       });
-    },
-    area_changeHandler: function (event) {
-      var target = $(event.target);
-      this.$el.toggleClass(target.data('class'), target.val() === '1');
-    },
-    feedback_changeHandler: function (event) {
-      this.$el.toggleClass('show-feedback-detail', event.target.value === '2' || event.target.value === '3');
+      this.$('.fetch-button').prop('disabled', true)
+        .find('i').addClass('fa-spin fa-spinner');
+      this.$('[name=ad_url]').prop('disabled', true);
     },
     fetchFile_successHandler: function (response) {
       alert('服务器抓取文件成功');
@@ -61,6 +95,9 @@
           this.$('[name=' + key + ']').val(response.form[key]);
         }
       }
+      this.$('.fetch-button').prop('disabled', false)
+        .find('i').removeClass('fa-spin fa-spinner');
+      this.$('[name=ad_url]').prop('disabled', false);
     },
     fetchFile_errorHandler: function (response) {
       console.log(response);
@@ -84,11 +121,11 @@
         }
       }
     },
-    platformButton_clickHandler: function (event) {
+    platform_changeHandler: function (event) {
       this.$el
         .removeClass('Android iPhone')
-        .addClass(event.target.innerText);
-      var is_ios = this.$el.hasClass('iPhone');
+        .addClass(event.target.labels[0].innerText);
+      var is_ios = event.target.value === '2';
       this.$('#process_name').prop('required', is_ios);
       $('#feedback').val(function () { return is_ios ? 4 : this.value});
       $('#app-uploader').data('accept', is_ios ? '*.ipa' : '*.apk');
