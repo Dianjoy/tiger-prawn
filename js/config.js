b1a2c4c1d7159b8dd258ff5342e307ca76e20fcb
qiujuan <qiujuan.yuan@dianjoy.com>
2015-02-10 16:19:52 +0800

    修改了页内加载时footer总是浮在上面的问题，修改了表格在移动端的适配

--- js/config.js
+++ js/config.js
@@ -2,7 +2,6 @@
  * Created by meathill on 14/11/13.
  */
 ;(function (ns) {
-  //ns.API = 'http://diytest.dianjoy.com/api/v1/'; //原来的地址为'http://ad-diy.com/'
   ns.API = 'http://ad-diy.com/';
   ns.UPLOAD = 'http://ad-diy.com/'; // 上传文件的起始路径
   ns.path = ''; // 项目路径
