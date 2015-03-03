045e6cf92d82d600c5b336d9338a081c52460f91
Meathill <meathill@gmail.com>
2015-03-03 18:24:02 +0800

    保存的时候返回干净的json

--- js/model/AD.js
+++ js/model/AD.js
@@ -51,13 +51,13 @@
       return Backbone.Model.prototype.save.call(this, key, value, options);
     },
     toJSON: function (options) {
-      var json = Backbone.Model.prototype.toJSON.call(this, options)
-        , previous = this.previousAttributes();
-      if (!_.isEmpty(previous)) {
-        json.previous = previous;
-      }
+      var json = Backbone.Model.prototype.toJSON.call(this, options);
       if (options) { // from sync，因为{patch: true}
         return json;
+      }
+      var previous = this.previousAttributes();
+      if (!_.isEmpty(previous)) {
+        json.previous = previous;
       }
       return _.extend(json, this.options);
     },
