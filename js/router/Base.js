c97ce7bfdb0e521816b17a1ec8691fefa3905632
Meathill <meathill@gmail.com>
2015-05-04 17:39:14 +0800

    在别的项目中引用的时候可以使用加载页

--- js/router/Base.js
+++ js/router/Base.js
@@ -24,7 +24,7 @@
         });
       }
       tp.config.login.api = this.$me.url;
-      this.$body.load('page/' + page + '.hbs', tp.config.login, {
+      this.$body.load(tp.path + 'page/' + page + '.hbs', tp.config.login, {
         isFull: true
       });
       this.$body.setFramework('login');
