d674eddfae6734152f9c90a3f7da22717fb770ea
csonchen <cson_chensheng@163.com>
2015-05-19 17:08:38 +0800

    修改手机串号导出popup页

--- js/page/DateCheck.js
+++ js/page/DateCheck.js
@@ -3,25 +3,17 @@
  */
 'use strict';
 (function (ns) {
-  var startDate;
   ns.DateCheck = tp.popup.Base.extend({
-    events: {
-      'dp.change #stat-range-start-date': 'startDate_changeHandler',
-      'dp.change #stat-range-end-date': 'endDate_changeHandler'
+    events: _.extend(tp.popup.Base.prototype.events, {
+      'dp.change input[name="start-date"]': 'startDate_changeHandler',
+      'dp.change input[name="end-date"]': 'endDate_changeHandler'
+    }),
+    startDate_changeHandler: function (event) {
+      $('input[name="end-date"]').data('DateTimePicker').minDate(event.date);
+      $('input[name="end-date"]').data('DateTimePicker').maxDate(moment(event.date).add(7, 'days'));
     },
-    startDate_changeHandler: function (e) {
-      startDate = e.target.value;
-    },
-    endDate_changeHandler: function (e) {
-      var endDate = e.target.value,
-          regularDate = new Date(Date.parse(startDate)).getTime() + 7*1000*60*60*24;
-      if (endDate < startDate) {
-        alert('亲，结束日期不得小于开始日期');
-      }
-      endDate = new Date(Date.parse(endDate)).getTime();
-      if (endDate > regularDate) {
-        alert('商务人员只能导出一周内的数据，如有需要，请联系技术');
-      }
+    endDate_changeHandler: function (event) {
+      $('input[name="start-date"]').data('DateTimePicker').maxDate(event.date);
     }
   });
 }(Nervenet.createNameSpace('tp.page')));
