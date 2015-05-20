b289d38e41fc0118e7a9fceb26e83ad09ec2c6ab
csonchen <cson_chensheng@163.com>
2015-05-20 10:31:22 +0800

    修改时间选择控件的小错误

--- js/page/DateCheck.js
+++ js/page/DateCheck.js
@@ -3,18 +3,28 @@
  */
 'use strict';
 (function (ns) {
+  var minDate,regularDate;
   ns.DateCheck = tp.popup.Base.extend({
     events: _.extend(tp.popup.Base.prototype.events, {
       'dp.change input[name="start-date"]': 'startDate_changeHandler',
       'dp.change input[name="end-date"]': 'endDate_changeHandler'
     }),
     startDate_changeHandler: function (event) {
-      $('input[name="end-date"]').data('DateTimePicker').minDate(event.date);
-      $('input[name="end-date"]').data('DateTimePicker').maxDate(moment(event.date).add(7, 'days'));
+      var endDate = $('input[name="end-date"]'),
+          startDate = event.date;
+      if (endDate.val() != "") {
+        if (moment(startDate) < minDate) {
+          endDate.val(moment(startDate).add(7, 'days').format('YYYY-MM-DD'));
+        } else if (moment(startDate) > regularDate) {
+          endDate.val(moment(startDate).format('YYYY-MM-DD'));
+        }
+      }
+      endDate.data('DateTimePicker').minDate(startDate);
+      endDate.data('DateTimePicker').maxDate(moment(startDate).add(7, 'days'));
     },
     endDate_changeHandler: function (event) {
-      $('input[name="start-date"]').data('DateTimePicker').minDate(moment(event.date).subtract(7, 'days'));
-      $('input[name="start-date"]').data('DateTimePicker').maxDate(event.date)
+      minDate = moment(event.date).subtract(7, 'days');
+      regularDate = moment(event.date);
     }
   });
 }(Nervenet.createNameSpace('tp.page')));
