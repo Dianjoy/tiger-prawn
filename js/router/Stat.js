a1579c993c2ea48b09b3bb5748c6af4ba1c92d25
csonchen <cson_chensheng@163.com>
2015-07-09 11:11:53 +0800

    初步完成广告数据统计页面

--- js/router/Stat.js
+++ js/router/Stat.js
@@ -9,7 +9,8 @@
       'stat(/)': 'showStat',
       'stat/:id': 'showADStat',
       'stat/:id/:date': 'showADStatDate',
-      'admin/stat/': 'showAdminADStat'
+      'admin/stat/': 'showAdminADStat',
+      'admin/stat/daily-ad/:id/:start/:end': 'showDailyADStat'
     },
     showADStat: function (id) {
       var model = new tp.model.AD({
@@ -35,7 +36,11 @@
     },
     showAdminADStat: function () {
       this.$body.load('page/stat/admin-list.html');
-      this.$body.setFramework('has-date-range', '广告数据分析');
+      this.$body.setFramework('has-date-range admin-list', '广告数据分析');
+    },
+    showDailyADStat: function (id, start, end) {
+      this.$body.load('page/stat/admin-daily.hbs', {id: id, start: start, end: end, API: tp.API});
+      this.$body.setFramework('has-date-range admin-daily', '广告统计/广告数据分析');
     }
   });
 }(Nervenet.createNameSpace('tp.router')));
