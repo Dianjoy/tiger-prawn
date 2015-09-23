807d8c746036d162043ba05744f796b499abf277
zombiezlk <zombiezlk@gmail.com>
2015-09-23 15:40:14 +0800

    只让最初窗口发起消息请求

--- js/model/Notice.js
+++ js/model/Notice.js
@@ -21,13 +21,29 @@
         },
         remove: false
       }, options);
-      Backbone.Collection.prototype.fetch.call(this, options);
+      var obj = JSON.parse(localStorage.getItem(tp.PROJECT + '#/notice/'));
+      if (obj) {
+        if (moment().diff(obj.time) > TIMEOUT) {
+          obj.time = moment().format();
+          localStorage.setItem(tp.PROJECT + '#/notice/', JSON.stringify(obj));
+          Backbone.Collection.prototype.fetch.call(this, options);
+        } else {
+          this.trigger('sync');
+          this.reset(obj.notice);
+        }
+      } else {
+        Backbone.Collection.prototype.fetch.call(this, options);
+      }
     },
     parse: function (response) {
       for (var i = 0, len = response.list.length; i < len; i++) {
         response.list[i].create_time = response.list[i].create_time.substr(5, 11);
         this.latest = response.list[i].id > this.latest ? response.list[i].id : this.latest;
       }
+      localStorage.setItem(tp.PROJECT + '#/notice/', JSON.stringify({
+        notice: response.list,
+        time: moment().format()
+      }));
       return response.list;
     },
     stop: function () {
