290c4ed856a9e2009b756838f04fe58737e91951
Meathill <meathill@gmail.com>
2015-11-17 16:49:15 +0800

    使用force参数避免报错

--- js/router/Ad.js
+++ js/router/Ad.js
@@ -24,7 +24,7 @@
           className: 'ad ad-new',
           loader: this.$me.isCP() ? null : tp.page.AdEditor
         };
-      this.$context.mapValue('ad', model);
+      this.$context.mapValue('ad', model, true);
       this.$body
         .load('page/ad/edit' + page + '.hbs', model, options)
         .setFramework('ad ad-new', '创建投放计划');
