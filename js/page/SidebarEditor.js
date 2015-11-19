76a551ec915a2398ba079ae73e106f1da3c9ea0e
zombiezlk <zombiezlk@gmail.com>
2015-11-19 18:02:06 +0800

    修改一些功能

--- js/page/SidebarEditor.js
+++ js/page/SidebarEditor.js
@@ -1,10 +1,14 @@
 'use strict';
 (function (ns) {
-  var key = tp.PROJECT + '-menu-list';
+  var key = tp.PROJECT + '-menu-list'
+    , eyesSelector = '.sidebar-nav-item, .sidebar-nav-item ul li'
+    , buttonSelector = '#menu-collapse, #menu-edit, #menu-search-button';
   ns.SidebarEditor = Backbone.View.extend({
     events: {
       'click #eye-edit-button': 'eyeEditButton_clickHandler',
       'click #menu-edit-button': 'menuEditButton_clickHandler',
+      'click #edit-confirm-button': 'editConfirmButton_clickHandler',
+      'click #edit-cancel-button': 'editCancelButton_clickHandler',
       'click #menu-search-button': 'menuSearchButton_clickHandler',
       'click #search-clear-button': 'searchClearButton_clickHandler',
       'click #menu-collapse-button': 'menuCollapseButton_clickHandler',
@@ -15,43 +19,71 @@
       this.manageSidebar();
     },
     eyeEditButton_clickHandler: function (event) {
-      var target = event.currentTarget
-        , item = $(target).parents('.sidebar-nav-item').attr('id')
-        , store = localStorage.getItem(key)
-        , menuList = JSON.parse(store);
-      $(target).toggleClass('slash');
-      $(target).hasClass('slash') ? menuList.push(item) : menuList.splice(menuList.indexOf(item), 1);
-      localStorage.setItem(key, JSON.stringify(menuList));
+      $(event.currentTarget).toggleClass('slash');
+      event.preventDefault();
       event.stopPropagation();
     },
     menuEditButton_clickHandler: function () {
-      $('body').toggleClass('sidebar-editing');
-      $('body').hasClass('sidebar-editing') ? $('.sidebar-nav-item').removeClass('hidden') : this.manageSidebar();
-    },
-    menuSearchButton_clickHandler: function (event) {
-      $(event.currentTarget).fadeOut(function () {
-        $('#menu-search form').fadeIn(function () {
-          $(this).find('input').focus();
-        });
+      $('body').addClass('sidebar-editing');
+      $(eyesSelector).removeClass('hidden').find('.collapse').collapse('show');
+      $('#menu-collapse, #menu-search').fadeOut(function () {
+        $('#edit-confirm, #edit-cancel').fadeIn();
       });
     },
-    searchClearButton_clickHandler: function () {
-      $('#menu-search-input').val('').focus().trigger('keyup');
+    editConfirmButton_clickHandler: function () {
+      var store = localStorage.getItem(key)
+        , menuList = JSON.parse(store);
+      $('body').removeClass('sidebar-editing');
+      $('#edit-confirm, #edit-cancel').fadeOut(function () {
+        $('#menu-collapse, #menu-search').fadeIn();
+      });
+      $(eyesSelector).each(function () {
+        var eye = $(this).find('#eye-edit-button')
+          , item = $(this).attr('id');
+        if (eye.hasClass('slash')) {
+          if (menuList.indexOf(item) === -1) {
+            menuList.push(item);
+          }
+        } else if (menuList.indexOf(item) !== -1) {
+          menuList.splice(menuList.indexOf(item), 1);
+        }
+      });
+      localStorage.setItem(key, JSON.stringify(menuList));
+      this.manageSidebar();
+    },
+    editCancelButton_clickHandler: function () {
+      $('body').removeClass('sidebar-editing');
+      $('#edit-confirm, #edit-cancel').fadeOut(function () {
+        $('#menu-collapse, #menu-search').fadeIn();
+      });
+      this.manageSidebar();
+    },
+    menuSearchButton_clickHandler: function () {
+      $(buttonSelector).fadeOut(function () {
+        $('#menu-search form').fadeIn(function () {
+          $('#menu-search').css('width', '100%').find('input').focus();
+        });
+      });
     },
     menuSearchInput_blurHandler: function (event) {
       var val = $(event.currentTarget).val();
       if (!val) {
         $('#menu-search form').fadeOut(function () {
-          $('#menu-search-button').fadeIn();
+          $('#menu-search').css('width', '30%');
+          $(buttonSelector).fadeIn();
         });
       }
+    },
+    searchClearButton_clickHandler: function () {
+      $('#menu-search-input').val('').focus().trigger('keyup');
     },
     menuSearchInput_keyupHandler: function (event) {
       var val = $(event.currentTarget).val() || '\\S'
         , reg = new RegExp(val + '+');
-      $('.sidebar-nav-item').each(function () {
+      $('.sidebar-nav-item ul').collapse('show');
+      $(eyesSelector).each(function () {
         var title = $(this).find('.title').text()
-          , isSlash = $(this).find('a .fa-eye').hasClass('slash');
+          , isSlash = $(this).find('#eye-edit-button').hasClass('slash');
         if (!isSlash) {
           reg.test(title) ? $(this).removeClass('hidden') : $(this).addClass('hidden');
         }
@@ -59,6 +91,7 @@
     },
     menuCollapseButton_clickHandler: function () {
       $('body').toggleClass('sidebar-collapsed');
+      $('#menu-search, #menu-edit').toggleClass('hidden');
       $('.sidebar-nav-item > a').each(function () {
         $(this).attr('data-toggle') ? $(this).removeAttr('data-toggle') : $(this).attr('data-toggle', 'collapse');
         $(this).siblings('.nav').removeAttr('style');
@@ -78,12 +111,13 @@
       var store = localStorage.getItem(key);
       if (store) {
         var menuList = JSON.parse(store);
-        _.each(menuList, function (element) {
-          $('#' + element).addClass('hidden').find('.fa-eye').addClass('slash');
+        $(eyesSelector).each(function () {
+          var eye = $(this).find('#eye-edit-button')
+            , item = $(this).attr('id')
+            , isExist = menuList.indexOf(item);
+          isExist === -1 ? eye.removeClass('slash') : $(this).addClass('hidden').find('#eye-edit-button').addClass('slash');
         });
-      } else {
-        localStorage.setItem(key, JSON.stringify([]));
-      }
+      } else { localStorage.setItem(key, JSON.stringify([])); }
     }
   });
 }(Nervenet.createNameSpace('tp.page')));
