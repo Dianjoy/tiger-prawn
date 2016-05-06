9dab7fd4bad87b080a2de6d143747043516e6ab8
zombiezlk <zombiezlk@gmail.com>
2016-05-06 12:57:42 +0800

    修一个bug

--- js/component/SmartTable.js
+++ js/component/SmartTable.js
@@ -250,7 +250,7 @@
       options = _.omit(options, 'unset') || {};
       this.refresh(options);
 
-      if ('start' in model.changed || 'end' in model.changed) {
+      if (this.collection.model && ('start' in model.changed || 'end' in model.changed)) {
         _.extend(this.collection.model.prototype.defaults, _.pick(model.changed, 'start', 'end'));
       }
       this.$el.addClass('loading');
