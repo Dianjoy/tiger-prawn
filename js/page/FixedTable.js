b98831b425533c12d9a5063f3092c47d0a215ff7
zombiezlk <zombiezlk@gmail.com>
2016-11-02 17:40:27 +0800

    需求 #56014

---
+++ js/page/FixedTable.js
@@ -0,0 +1,19 @@
+'use strict';
+(function (ns, _, Backbone) {
+  ns.FixedTable = Backbone.View.extend({
+    initialize: function () {
+      this.scrollHandler = _.bind(this.scrollHandler, this);
+      $(window).scroll(this.scrollHandler);
+    },
+    scrollHandler: function () {
+      var scroll = document.body.scrollTop + 70
+        , fixTable = this.$('.fix-table');
+      fixTable.toggleClass('fixed', scroll > this.$('.smart-table').offset().top);
+      if (fixTable.hasClass('fixed')) {
+        fixTable.width(this.$('.smart-table').width());
+      } else {
+        fixTable.removeAttr('style');
+      }
+    }
+  });
+})(Nervenet.createNameSpace('tp.page'), _, Backbone);
